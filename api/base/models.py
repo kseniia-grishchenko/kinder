591c07ea317017622b9b466ae08898bad1197944
Nataliia Zakharchuk <62181026+Nattalli@users.noreply.github.com>
2021-09-12 17:50:23 +0300

    add models (#1)

--- api/base/models.py
+++ api/base/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+SEX_CHOICES = (
+    ('Male', 'Male'),
+    ('Female', 'Female'),
+    ('Not chosen', 'Not chosen')
+)
+
+
+class CustomUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.TextField(blank=False, max_length=30)
+    age = models.IntegerField(null=True, default=18)
+    sex = models.CharField(choices=SEX_CHOICES, default='Not chosen', max_length=20)
+    location = models.CharField(max_length=50)
+    contact = models.CharField(max_length=30)
+    photo = models.ImageField(default='image/default.jpg')
+    description = models.TextField(max_length=500, blank=True)
+    budget = models.IntegerField(blank=True, null=True)
+
+
+class UserRelationship(models.Model):
+    initiator = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='initiator')
+    receiver = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='receiver')
+    got_connected = models.BooleanField(default=False)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class UserTag(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
