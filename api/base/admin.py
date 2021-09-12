4f02c4675774b9785babdd94b4e47a465c4c2c9c
kseniia-grishchenko <61903516+kseniia-grishchenko@users.noreply.github.com>
2021-09-12 18:14:28 +0300

    Revert "add models (#1)"
    This reverts commit 591c07ea317017622b9b466ae08898bad1197944.

--- api/base/admin.py
+++ api/base/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import *
 
-
-admin.site.register(CustomUser)
-admin.site.register(UserTag)
-admin.site.register(UserRelationship)
-admin.site.register(Tag)
+# Register your models here.
