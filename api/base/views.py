0f0444bbc5da3940a50f3026e10cdfe279d45cee
Nataliia Zakharchuk <62181026+Nattalli@users.noreply.github.com>
2021-09-15 08:45:18 +0300

    Changes (#7)
    * added user-followers
    
    * fixed password changing
    
    * open contacts if mutually subscribed
    
    * change pages style and fixed 'subscribe-unsubscribe' button

--- api/base/views.py
+++ api/base/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponseRedirect
 from .forms import UserForm, CustomUserRegisterForm, LoginForm, CustomUserForm
 from .models import CustomUser
 from django.contrib.auth.models import User
+from django.urls import reverse_lazy
 
 
 def register_user(request):
@@ -47,15 +48,23 @@ def login_user(request):
 def list_all(request):
     context = {}
     users = CustomUser.objects.select_related('user').all()
+    if request.user.is_authenticated:
+        user = User.objects.get(id=request.user.id)
+        users = users.exclude(user=user)
     context['users'] = users
     return render(request, 'home.html', context)
 
 
 def get_user_profile(request, id):
     context = {}
+    current_user = CustomUser.objects.select_related('user').get(user=request.user)
     user = User.objects.get(id=id)
-    custom_user = CustomUser.objects.get(user=user)
+    custom_user = CustomUser.objects.select_related('user').get(user=user)
     context['user'] = custom_user
+    subscribed = custom_user in current_user.subscriptions.all()
+    mutually_subscribed = subscribed and custom_user in current_user.followers.all()
+    context['subscribed'] = subscribed
+    context['mutually_subscribed'] = mutually_subscribed
     return render(request, 'profile.html', context)
 
 
@@ -78,7 +87,7 @@ def change_password(request):
             user = form.save()
             update_session_auth_hash(request, user)  # Important!
             messages.success(request, 'Your password was successfully updated!')
-            return redirect('change_password')
+            return redirect('users')
         else:
             messages.error(request, 'Please correct the error below.')
     else:
@@ -88,6 +97,8 @@ def change_password(request):
 
 
 def update_relationship(request, id):
+    if request.user.id == int(id):
+        return redirect('/')
     sender = CustomUser.objects.get(user=request.user.id)
 
     if request.method == "POST":
@@ -105,8 +116,16 @@ def update_relationship(request, id):
 
 
 def user_subscriptions(request, id):
-    user_id = id
-    user = User.objects.get(id=user_id)
+    user = CustomUser.objects.get(user_id=id)
     subscriptions = user.subscriptions.all()
-    context = {"subscriptions": subscriptions, "username": user.username, "user_id": id}
+    context = {"subscriptions": subscriptions, "username": user.first_name,
+               "user_id": user.id, "len": len(subscriptions)}
     return render(request, 'subscriptions.html', context)
+
+
+def user_followers(request, id):
+    user = CustomUser.objects.get(user_id=id)
+    followers = user.followers.all()
+    context = {"followers": followers, "username": user.first_name,
+               "user_id": id, "len": len(followers)}
+    return render(request, 'followers.html', context)
