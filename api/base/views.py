bbd81181eb9cb91c0b8ff42c256e3c7c57361dc3
kseniia <kseniia@smartiot.cloud>
2021-09-25 20:49:26 +0300

    update using django-rest framework

--- api/base/views.py
+++ api/base/views.py
@@ -2,130 +2,167 @@ from django.contrib import messages
 from django.contrib.auth.forms import PasswordChangeForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, update_session_auth_hash
-from django.http import HttpResponseRedirect
+from rest_framework.decorators import api_view, permission_classes
 from .forms import UserForm, CustomUserRegisterForm, LoginForm, CustomUserForm
+from rest_framework import status
+from rest_framework.response import Response
+from django.contrib.auth.hashers import make_password
 from .models import CustomUser
 from django.contrib.auth.models import User
-from django.urls import reverse_lazy
+from base.serializers import UserSerializer, CustomUserSerializer, UserSerializerWithToken
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        serializer = UserSerializerWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(['POST'])
 def register_user(request):
-    context = {}
+    data = request.data
 
-    user_form = UserForm(request.POST or None)
-    custom_user_form = CustomUserRegisterForm(request.POST or None)
-
-    if all((user_form.is_valid(), custom_user_form.is_valid())):
-        user = user_form.save()
-        password = user_form.cleaned_data['password']
-        user.set_password(password)
-        user = user_form.save()
-        custom_user = custom_user_form.save(commit=False)
-        custom_user.user = user
-        custom_user.save()
-
-    context['user_form'] = user_form
-    context['custom_user_form'] = custom_user_form
-    return render(request, "register_view.html", context)
-
-
-def login_user(request):
-    context = {}
-    form = LoginForm(request.POST or None)
-    if form.is_valid():
-        username = form.cleaned_data['username']
-        password = form.cleaned_data['password']
-        user = authenticate(
-            username=username, password=password
+    try:
+        user = User.objects.create(
+            username=data['username'],
+            email=data['email'],
+            password=make_password(data['password'])
         )
-        if user:
-            login(request, user)
-            return HttpResponseRedirect('/')
-    context['form'] = form
-    return render(request, 'login.html', context)
+        custom_user = CustomUser.objects.get(user=user)
+        serializer = CustomUserSerializer(custom_user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail': 'User already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(['GET'])
 def list_all(request):
-    context = {}
     users = CustomUser.objects.select_related('user').all()
     if request.user.is_authenticated:
         user = User.objects.get(id=request.user.id)
         users = users.exclude(user=user)
-    context['users'] = users
-    return render(request, 'home.html', context)
+    serializer = CustomUserSerializer(users, many=True)
+    return Response(serializer.data)
 
 
+@api_view(['GET'])
 def get_user_profile(request, id):
-    context = {}
-    current_user = CustomUser.objects.select_related('user').get(user=request.user)
-    user = User.objects.get(id=id)
-    custom_user = CustomUser.objects.select_related('user').get(user=user)
-    context['user'] = custom_user
-    subscribed = custom_user in current_user.subscriptions.all()
-    mutually_subscribed = subscribed and custom_user in current_user.followers.all()
-    context['subscribed'] = subscribed
-    context['mutually_subscribed'] = mutually_subscribed
-    return render(request, 'profile.html', context)
+    try:
+        user = User.objects.get(id=id)
+        custom_user = CustomUser.objects.select_related('user').get(user=user)
+        serializer = CustomUserSerializer(custom_user, many=False)
+        if request.user.is_authenticated:
+            current_user = CustomUser.objects.select_related('user').get(user=request.user)
+            subscribed = custom_user in current_user.subscriptions.all()
+            mutually_subscribed = subscribed and custom_user in current_user.followers.all()
+            serializer.data.subscribed = subscribed
+            serializer.data.mutually_subscribed = mutually_subscribed
+        return Response(serializer.data)
+    except:
+        message = {'detail': 'User does not exist'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_my_profile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
 def update_profile(request):
-    context = {}
-    custom_user = CustomUser.objects.select_related('user').get(user=request.user)
-    form = CustomUserForm(request.POST or None, instance=custom_user)
-    if form.is_valid():
-        form.save()
-        return redirect('/')
-    context['form'] = form
-    return render(request, 'my_profile.html', context)
+    user = request.user
+
+    data = request.data
+
+    user.username = data['username'] or request.user.username
+    user.email = data['email'] or request.user.email
+
+    custom_user = CustomUser.objects.select_related('user').get(user=user)
+    custom_user.first_name = data['first_name'] or custom_user.first_name
+    custom_user.sex = data['sex'] or custom_user.sex
+    custom_user.location = data['location'] or custom_user.location
+    custom_user.budget = data['budget'] or custom_user.budget
+    custom_user.description = data['description'] or custom_user.description
+    custom_user.contact = data['contact'] or custom_user.contact
+
+    serializer = CustomUserSerializer(custom_user, many=False, partial=True)
+    if data['password'] != '':
+        user.password = make_password(data['password'])
+
+    user.save()
+    custom_user.save()
+    return Response(serializer.data)
 
 
 def change_password(request):
-    context = {}
-    if request.method == 'POST':
-        form = PasswordChangeForm(request.user, request.POST)
-        if form.is_valid():
-            user = form.save()
-            update_session_auth_hash(request, user)  # Important!
-            messages.success(request, 'Your password was successfully updated!')
-            return redirect('users')
-        else:
-            messages.error(request, 'Please correct the error below.')
-    else:
-        form = PasswordChangeForm(request.user)
-    context['form'] = form
-    return render(request, 'password_change.html', context)
+    # context = {}
+    # if request.method == 'POST':
+    #     form = PasswordChangeForm(request.user, request.POST)
+    #     if form.is_valid():
+    #         user = form.save()
+    #         update_session_auth_hash(request, user)  # Important!
+    #         messages.success(request, 'Your password was successfully updated!')
+    #         return redirect('users')
+    #     else:
+    #         messages.error(request, 'Please correct the error below.')
+    # else:
+    #     form = PasswordChangeForm(request.user)
+    # context['form'] = form
+    # return render(request, 'password_change.html', context)
+    pass
 
 
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def update_relationship(request, id):
     if request.user.id == int(id):
-        return redirect('/')
+        return
     sender = CustomUser.objects.get(user=request.user.id)
 
-    if request.method == "POST":
-        user = User.objects.get(id=id)
-        receiver = CustomUser.objects.get(user=user)
-        subscriptions = sender.subscriptions.all()
-        if receiver in subscriptions:
-            sender.subscriptions.remove(receiver)
-            receiver.followers.remove(sender)
-        else:
-            sender.subscriptions.add(receiver)
-            receiver.followers.add(sender)
+    user = User.objects.get(id=id)
+    receiver = CustomUser.objects.get(user=user)
+    subscriptions = sender.subscriptions.all()
+    if receiver in subscriptions:
+        sender.subscriptions.remove(receiver)
+        receiver.followers.remove(sender)
+    else:
+        sender.subscriptions.add(receiver)
+        receiver.followers.add(sender)
 
-    return redirect('users')
+    serializer = CustomUserSerializer(receiver, many=False)
+    return Response(serializer.data)
 
 
+@api_view(['GET'])
 def user_subscriptions(request, id):
     user = CustomUser.objects.get(user_id=id)
     subscriptions = user.subscriptions.all()
-    context = {"subscriptions": subscriptions, "username": user.first_name,
-               "user_id": user.id, "len": len(subscriptions)}
-    return render(request, 'subscriptions.html', context)
+    serializer = CustomUserSerializer(subscriptions, many=True)
+    return Response(serializer.data)
 
 
+@api_view(['GET'])
 def user_followers(request, id):
     user = CustomUser.objects.get(user_id=id)
     followers = user.followers.all()
-    context = {"followers": followers, "username": user.first_name,
-               "user_id": id, "len": len(followers)}
-    return render(request, 'followers.html', context)
+    serializer = CustomUserSerializer(followers, many=True)
+    return Response(serializer.data)
+
