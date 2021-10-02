401ed75854d2100b1e190d42b2b4fc5b84be681a
kseniia-grishchenko <kseniarctybz@gmail.com>
2021-10-02 23:57:54 +0300

    add static root

--- api/api/settings.py
+++ api/api/settings.py
@@ -166,6 +166,11 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
