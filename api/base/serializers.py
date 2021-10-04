bdfa4f4689b9f55fe8f7d99e0ec8f4ae249516b5
kseniia-grishchenko <kseniarctybz@gmail.com>
2021-10-04 17:56:43 +0300

    change import order

--- api/base/serializers.py
+++ api/base/serializers.py
@@ -1,7 +1,8 @@
-from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import CustomUser
+from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken
+
+from .models import CustomUser
 
 
 class UserSerializer(serializers.ModelSerializer):
