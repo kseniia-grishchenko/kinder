d0f94bf7dce73190d51c917504dddc47fd50213c
kseniia <kseniia@smartiot.cloud>
2021-10-17 00:14:59 +0300

    Fixed allowed hosts

--- api/api/settings.py
+++ api/api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'kinder-tinder.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'kinder-tinder.herokuapp.com']
 
 # Application definition
 
@@ -182,6 +182,3 @@ CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000'
 )
-
-if os.getcwd() == '/app':
-    DEBUG = False
