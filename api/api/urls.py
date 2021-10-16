8839d7373ed5052527712288a7158f476b0508a9
kseniia <kseniia@smartiot.cloud>
2021-10-16 23:30:34 +0300

    prepare all files for deploy

--- api/api/urls.py
+++ api/api/urls.py
@@ -3,12 +3,15 @@ from django.urls import path
 from django.conf.urls import include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('', include('base.urls')),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('api/', include('base.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- api/ui/src/App.js
+++ api/ui/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "antd/dist/antd.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/header/header";
 import Home from "./views/home/home";
 import Login from "./views/login/login";
