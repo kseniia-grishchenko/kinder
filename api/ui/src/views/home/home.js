b3012b1095efe19ecbaad16878dc8b3a27851873
kseniia <kseniia@smartiot.cloud>
2021-11-10 21:50:39 +0200

    search user by tags

--- api/ui/src/views/home/home.js
+++ api/ui/src/views/home/home.js
@@ -2,29 +2,73 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import './home.css'
 import UserCard from '../../components/userCard/userCard'
+import { Input } from 'antd';
 
 const Home = () => {
   const [users, setUsers] = useState([])
+  const [text, setText] = useState("");
+  const user = JSON.parse(localStorage.getItem('user'))
+
+    const config = {
+      headers: {
+          'Content-type': 'application/json',
+          Authorization: `Bearer ${user.token}`
+      }
+  }
 
   useEffect(() => {
-    const currentUser = JSON.parse(localStorage.getItem('user')) || null
     const getUsers = async () => {
-      const { data: users_from_db } = await axios.get(
-        '/users/'
+      const { data: usersFromDb } = await axios.get(
+        '/users/',
+          config
       )
-      currentUser
-        ? setUsers(users_from_db.filter((user) => user.user !== currentUser.id))
-        : setUsers(users_from_db)
+      user
+        ? setUsers(usersFromDb.filter((user) => user.user !== user.id))
+        : setUsers(usersFromDb)
     }
 
     getUsers().catch((err) => console.log(err))
   }, [])
 
+    const onSearchHandler = async (text) =>{
+      if(text === '') {
+          const { data: usersFromDb } = await axios.get(
+            '/users/',
+              config
+          )
+          setText(text);
+          setUsers((usersFromDb))
+          return
+      }
+    if(text.length > 3){
+        const { data: suggested_users } = await axios.get(
+          `/users/${text}`,
+            config
+        )
+        setUsers(suggested_users);
+    }
+    setText(text);
+
+}
+
   return (
-    <div id='users'>
-      {users?.map((user) => (
-        <UserCard user={user} key={user.id} />
-      ))}
+    <div>
+        <div style={{padding: '10px 50px'}}>
+            <Input placeholder="input search text"
+                   onChange={e => {onSearchHandler(e.target.value)}}
+                   value={text}
+                   onBlur={() => {
+                    setTimeout(() => {
+                        setUsers([])
+                    }, 25)
+                }}
+            />
+        </div>
+      <div id='users'>
+        {users?.map((user) => (
+          <UserCard user={user} key={user.id} />
+        ))}
+      </div>
     </div>
   )
 }
